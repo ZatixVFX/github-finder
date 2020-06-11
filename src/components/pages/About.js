593fd4376d870873331dfb8af079219392fd53e6
Junaid Salie <ZatixVFX@users.noreply.github.com>
2020-06-11 14:43:56 -0700

    commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from "react";
+
+export const About = () => {
+  return (
+    <Fragment>
+      <h1>About this App</h1>
+      <p>App to search Github users</p>
+    </Fragment>
+  );
+};
+export default About;
